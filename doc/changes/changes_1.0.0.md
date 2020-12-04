c3f39198602d9318ad9170a704ba74ca9a397d6d
Muhammet Orazov <m.orazow@gmail.com>
2020-12-04 15:17:02 +0100

    #11: Added project keeper maven plugin (#17)

---
+++ doc/changes/changes_1.0.0.md
@@ -0,0 +1,15 @@
+# Kafka Connect JDBC EXASOL 1.0.0, released 2020-??-??
+
+## Code name: 
+
+## Features / Enhancements
+
+* #20: Added project-keeper maven plugin (PR #21)
+
+## Dependency Updates
+
+* Removed `org.apache.maven.plugins:maven-checkstyle-plugin` plugin
+* Updated to `com.exasol:exasol-jdbc:7.0.4` (was `6.2.5`)
+* Updated to `io.confluent:kafka-connect-jdbc:5.5.2` (was `5.5.0`)
+* Updated to `org.apache.kafka:connect-api:2.6.0` (was `2.5.0`)
+* Updated to `org.jacoco:org.jacoco.agent:0.8.6` (was `0.8.5`)
--- src/main/java/com/exasol/connect/jdbc/dialect/ExasolDatabaseDialect.java
+++ src/main/java/com/exasol/connect/jdbc/dialect/ExasolDatabaseDialect.java
@@ -25,151 +25,122 @@ import io.confluent.connect.jdbc.util.TableId;
  */
 public class ExasolDatabaseDialect extends GenericDatabaseDialect {
 
-  /**
-   * The provider for {@link ExasolDatabaseDialect}.
-   */
-  public static class Provider extends SubprotocolBasedProvider {
-    public Provider() {
-      super(ExasolDatabaseDialect.class.getSimpleName(), "exa");
+    /**
+     * The provider for {@link ExasolDatabaseDialect}.
+     */
+    public static class Provider extends SubprotocolBasedProvider {
+        public Provider() {
+            super(ExasolDatabaseDialect.class.getSimpleName(), "exa");
+        }
+
+        @Override
+        public DatabaseDialect create(AbstractConfig config) {
+            return new ExasolDatabaseDialect(config);
+        }
+    }
+
+    /**
+     * Create a new dialect instance with the given connector configuration.
+     *
+     * @param config the connector configuration; may not be null
+     */
+    public ExasolDatabaseDialect(AbstractConfig config) {
+        super(config, new IdentifierRules(".", "\"", "\""));
     }
 
     @Override
-    public DatabaseDialect create(AbstractConfig config) {
-      return new ExasolDatabaseDialect(config);
-    }
-  }
-
-  /**
-   * Create a new dialect instance with the given connector configuration.
-   *
-   * @param config the connector configuration; may not be null
-   */
-  public ExasolDatabaseDialect(AbstractConfig config) {
-    super(config, new IdentifierRules(".", "\"", "\""));
-  }
-
-  @Override
-  protected String getSqlType(SinkRecordField field) {
-    if (field.schemaName() != null) {
-      switch (field.schemaName()) {
-        case Decimal.LOGICAL_NAME:
-          return "DECIMAL(36," + field.schemaParameters().get(Decimal.SCALE_FIELD) + ")";
-        case Date.LOGICAL_NAME:
-          return "DATE";
-        // case Time.LOGICAL_NAME:
-        // TIME is not supported (-> INT32)
-        case Timestamp.LOGICAL_NAME:
-          return "TIMESTAMP";
+    protected String getSqlType(SinkRecordField field) {
+        if (field.schemaName() != null) {
+            switch (field.schemaName()) {
+            case Decimal.LOGICAL_NAME:
+                return "DECIMAL(36," + field.schemaParameters().get(Decimal.SCALE_FIELD) + ")";
+            case Date.LOGICAL_NAME:
+                return "DATE";
+            case Timestamp.LOGICAL_NAME:
+                return "TIMESTAMP";
+            default:
+                // fall through to normal types
+            }
+        }
+        switch (field.schemaType()) {
+        case INT8:
+            return "DECIMAL(3,0)";
+        case INT16:
+            return "DECIMAL(5,0)";
+        case INT32:
+            return "DECIMAL(10,0)";
+        case INT64:
+            return "DECIMAL(19,0)";
+        case FLOAT32:
+            return "FLOAT";
+        case FLOAT64:
+            return "DOUBLE";
+        case BOOLEAN:
+            return "BOOLEAN";
+        case STRING:
+            return "CLOB";
         default:
-          // fall through to normal types
-      }
+            return super.getSqlType(field);
+        }
     }
-    switch (field.schemaType()) {
-      case INT8:
-        return "DECIMAL(3,0)";
-      case INT16:
-        return "DECIMAL(5,0)";
-      case INT32:
-        return "DECIMAL(10,0)";
-      case INT64:
-        return "DECIMAL(19,0)";
-      case FLOAT32:
-        return "FLOAT";
-      case FLOAT64:
-        return "DOUBLE";
-      case BOOLEAN:
-        return "BOOLEAN";
-      case STRING:
-        return "CLOB";
-      // case BYTES:
-      // BLOB is not supported
-      default:
-        return super.getSqlType(field);
+
+    @Override
+    public String buildDropTableStatement(TableId table, DropOptions options) {
+        ExpressionBuilder builder = expressionBuilder();
+
+        builder.append("DROP TABLE");
+        if (options.ifExists()) {
+            builder.append(" IF EXISTS");
+        }
+        builder.append(" " + table);
+        if (options.cascade()) {
+            builder.append(" CASCADE CONSTRAINTS");
+        }
+        return builder.toString();
     }
-  }
 
-  @Override
-  public String buildDropTableStatement(
-      TableId table,
-      DropOptions options
-  ) {
-    ExpressionBuilder builder = expressionBuilder();
-
-    builder.append("DROP TABLE");
-    if (options.ifExists()) {
-      builder.append(" IF EXISTS");
+    @Override
+    public List<String> buildAlterTable(TableId table, Collection<SinkRecordField> fields) {
+        final List<String> queries = new ArrayList<>(fields.size());
+        for (SinkRecordField field : fields) {
+            queries.addAll(super.buildAlterTable(table, Collections.singleton(field)));
+        }
+        return queries;
     }
-    builder.append(" " + table);
-    if (options.cascade()) {
-      builder.append(" CASCADE CONSTRAINTS");
+
+    @Override
+    public String buildUpsertQueryStatement(TableId table, Collection<ColumnId> keyColumns,
+            Collection<ColumnId> nonKeyColumns) {
+        ExpressionBuilder builder = expressionBuilder();
+        builder.append("MERGE INTO ");
+        builder.append(table);
+        builder.append(" AS target USING (SELECT ");
+        builder.appendList().delimitedBy(", ").transformedBy(ExpressionBuilder.columnNamesWithPrefix("? AS "))
+                .of(keyColumns, nonKeyColumns);
+        builder.append(") AS incoming ON (");
+        builder.appendList().delimitedBy(" AND ").transformedBy(this::transformAs).of(keyColumns);
+        builder.append(")");
+        if (nonKeyColumns != null && !nonKeyColumns.isEmpty()) {
+            builder.append(" WHEN MATCHED THEN UPDATE SET ");
+            builder.appendList().delimitedBy(",").transformedBy(this::transformUpdate).of(nonKeyColumns);
+        }
+        builder.append(" WHEN NOT MATCHED THEN INSERT (");
+        builder.appendList().delimitedBy(",").transformedBy(ExpressionBuilder.columnNames()).of(nonKeyColumns,
+                keyColumns);
+        builder.append(") VALUES (");
+        builder.appendList().delimitedBy(",").transformedBy(ExpressionBuilder.columnNamesWithPrefix("incoming."))
+                .of(nonKeyColumns, keyColumns);
+        builder.append(")");
+        return builder.toString();
     }
-    return builder.toString();
-  }
 
-  @Override
-  public List<String> buildAlterTable(
-      TableId table,
-      Collection<SinkRecordField> fields
-  ) {
-    final List<String> queries = new ArrayList<>(fields.size());
-    for (SinkRecordField field : fields) {
-      queries.addAll(super.buildAlterTable(table, Collections.singleton(field)));
+    private void transformAs(ExpressionBuilder builder, ColumnId col) {
+        builder.append("target.").appendIdentifierQuoted(col.name()).append("=incoming.")
+                .appendIdentifierQuoted(col.name());
     }
-    return queries;
-  }
 
-  @Override
-  public String buildUpsertQueryStatement(
-      TableId table,
-      Collection<ColumnId> keyColumns,
-      Collection<ColumnId> nonKeyColumns
-  ) {
-    ExpressionBuilder builder = expressionBuilder();
-    builder.append("MERGE INTO ");
-    builder.append(table);
-    builder.append(" AS target USING (SELECT ");
-    builder.appendList()
-           .delimitedBy(", ")
-           .transformedBy(ExpressionBuilder.columnNamesWithPrefix("? AS "))
-           .of(keyColumns, nonKeyColumns);
-    builder.append(") AS incoming ON (");
-    builder.appendList()
-           .delimitedBy(" AND ")
-           .transformedBy(this::transformAs)
-           .of(keyColumns);
-    builder.append(")");
-    if (nonKeyColumns != null && !nonKeyColumns.isEmpty()) {
-      builder.append(" WHEN MATCHED THEN UPDATE SET ");
-      builder.appendList()
-             .delimitedBy(",")
-             .transformedBy(this::transformUpdate)
-             .of(nonKeyColumns);
+    private void transformUpdate(ExpressionBuilder builder, ColumnId col) {
+        builder.appendIdentifierQuoted(col.name()).append("=incoming.").appendIdentifierQuoted(col.name());
     }
-    builder.append(" WHEN NOT MATCHED THEN INSERT (");
-    builder.appendList()
-           .delimitedBy(",")
-           .transformedBy(ExpressionBuilder.columnNames())
-           .of(nonKeyColumns, keyColumns);
-    builder.append(") VALUES (");
-    builder.appendList()
-           .delimitedBy(",")
-           .transformedBy(ExpressionBuilder.columnNamesWithPrefix("incoming."))
-           .of(nonKeyColumns, keyColumns);
-    builder.append(")");
-    return builder.toString();
-  }
-
-  private void transformAs(ExpressionBuilder builder, ColumnId col) {
-    builder.append("target.")
-           .appendIdentifierQuoted(col.name())
-           .append("=incoming.")
-           .appendIdentifierQuoted(col.name());
-  }
-
-  private void transformUpdate(ExpressionBuilder builder, ColumnId col) {
-    builder.appendIdentifierQuoted(col.name())
-           .append("=incoming.")
-           .appendIdentifierQuoted(col.name());
-  }
 
 }
